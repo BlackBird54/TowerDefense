c3e4826f6fce85ea68589573af29a90bb44509e8
Ian <rathlos246@gmail.com>
2021-06-09 19:08:14 -0400

    Added project

---
+++ New Unity Project (3)/Assets/PlayerMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Transform PlayerPos;
+    public Vector2 velocity;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerPos = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float xMov = Input.GetAxis("X");
+        float yMov = Input.GetAxis("Y");
+        velocity = new Vector2(xMov, yMov);
+        velocity *= speed;
+        PlayerPos.position += new Vector3(velocity.x, velocity.y, 0f);
+    }
+}
